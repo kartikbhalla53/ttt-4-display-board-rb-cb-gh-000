ddeb2f0b8966af8753c319c8f375d6dcad9b57fc
Kartik Bhalla <kartikbhalla53@yahoo.com>
2018-04-07 11:14:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 board = [" "," "," "," ","X"," "," "," "," "]
 
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board(0)} |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
