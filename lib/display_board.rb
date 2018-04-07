b18fd39e74776cd14f0dce163a655eff585ae4b1
Kartik Bhalla <kartikbhalla53@yahoo.com>
2018-04-07 11:12:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,2 @@
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board([" "," "," "," "," "," "," "," "," "])
-  puts " #{board(0)} |  #{board(1)} |  #{board(2)} "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
+board = [" "," "," "," ","X"," "," "," "," "]
+display_board(board)
